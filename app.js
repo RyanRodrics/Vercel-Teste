b9ea35791359aa5ac446b0ae5258c7de750f30b4
Ryan Rodrigues <ryanpimentelbr@gmail.com>
2024-03-19 17:02:07 -0300

    update handlebars 3

--- app.js
+++ app.js
@@ -12,7 +12,10 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname, "public")));
 
 // Handlebars
-app.engine('handlebars', engine({ defaultLayout: 'main'}));
+app.engine('handlebars', engine({ 
+    defaultLayout: 'main', 
+    layoutsDir: path.join(__dirname, "views", "layouts")
+}));
 app.set('view engine', 'handlebars');
 app.set('views', './views');
 
