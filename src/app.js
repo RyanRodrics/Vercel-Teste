2d0a6b3a8bac211e149b85c5a18c2e58497be93e
Ryan Rodrigues <ryanpimentelbr@gmail.com>
2024-03-19 17:15:02 -0300

    edit src

--- src/app.js
+++ src/app.js
@@ -9,7 +9,7 @@ const __dirname = path.dirname(__filename);
 
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
-app.use(express.static(__dirname + '../../' + '/public'));
+app.use(express.static(__dirname + '../../' + 'public'));
 
 // Handlebars
 app.engine('handlebars', engine({ 
